5e70c68997bf0770a9aa06df6d4b31278614dce4
Cengizhan KOSE <cengizhan.kose@monsternotebook.com>
2024-03-10 08:42:32 +0300

    temporary data file

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const ChallengesData = [
+  {
+    id: 1,
+    image:
+      "https://pbs.twimg.com/profile_images/1595521438949187584/vxOaz0V3_400x400.jpg",
+    name: "Superteam Turkey",
+    category: "Coding",
+    title: "Python Challange",
+    date: "2022-12-31",
+    prize: 0.5,
+  },
+  {
+    id: 2,
+    image:
+      "https://pbs.twimg.com/profile_images/1595521438949187584/vxOaz0V3_400x400.jpg",
+    name: "Superteam Turkey",
+    category: "Social Media",
+    title: "Hackathon Photo Challange",
+    date: "2022-12-31",
+    prize: 1,
+  },
+  {
+    id: 3,
+    image:
+      "https://pbs.twimg.com/profile_images/1595521438949187584/vxOaz0V3_400x400.jpg",
+    name: "Superteam Turkey",
+    category: "Coding",
+    title: "Rust Challange",
+    date: "2022-12-31",
+    prize: 0.5,
+  },
+  {
+    id: 4,
+    image:
+      "https://pbs.twimg.com/profile_images/1595521438949187584/vxOaz0V3_400x400.jpg",
+    name: "Superteam Turkey",
+    category: "Social Media",
+    title: "Hackathon Photo Challange",
+    date: "2022-12-31",
+    prize: 1,
+  },
+];
